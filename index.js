bb4642424b3e2298fb74bec7c26e53635134c1e6
RaqueSouto <76965176+RaqueSouto@users.noreply.github.com>
2024-05-06 14:42:04 +0200

    creación endpoint GET

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const {tareas} = require("./db");
 
 
 const servidor = express();
@@ -10,6 +11,22 @@ if(process.env.LOCAL){
     servidor.use(express.static("./pruebas"));
 }
 
+servidor.get("/tareas", async (peticion,respuesta) => {
+    try{
+        let resultado = await tareas();
 
+        respuesta.json(resultado);
+
+    }catch(error){
+        respuesta.status(500);
+        
+        respuesta.json(error)
+    }
+})
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
+})
 
 servidor.listen(process.env.PORT);
